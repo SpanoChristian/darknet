6426435ad0554e0c0d97f93a1976caa88a78bd51
Christian Spano <c.spano@studenti.unibg.it>
2020-09-05 15:45:03 +0200

    Update image.c

--- src/image.c
+++ src/image.c
@@ -330,11 +330,11 @@ int compare_by_probs(const void *a_ptr, const void *b_ptr) {
 
 void draw_detections_v3(image im, detection *dets, int num, float thresh, char **names, image **alphabet, int classes, int ext_output)
 {
-    std::string s = file_name;
-    std::string delimiter = "_";
+    char s[strlen(file_name)] = file_name;
+    char delimiter[1] = "_";
 
-    std::string img_identification = s.substr(0, s.find(delimiter));
-    std::string img_camera = s.substr(1, s.find(delimiter));
+    char img_identification[5] = s.substr(0, s.find(delimiter));
+    char img_camera[5] = s.substr(1, s.find(delimiter));
 
     static int frame_id = 0;
     frame_id++;
