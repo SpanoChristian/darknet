167354ae246f5a332f9f12d18092acd1d8a676b3
Christian Spano <c.spano@studenti.unibg.it>
2020-09-13 10:21:57 +0200

    Update detector.c

--- src/detector.c
+++ src/detector.c
@@ -1647,8 +1647,9 @@ void test_detector(char *datacfg, char *cfgfile, char *weightfile, char *filenam
         if(letter_box) sized = letterbox_image(im, net.w, net.h);
         else sized = resize_image(im, net.w, net.h);
 
+        char path[];
         strncpy(path,filename,100); /* copy up to 100 chars from field to c */
-        c[99] = '\0';         /* ..and make sure the last char in c is '\0' */
+        path[99] = '\0';         /* ..and make sure the last char in c is '\0' */
         //char path[] = filename; //string with escaped slashes
         char temp[256]; //result here
         char *ch; //define this
